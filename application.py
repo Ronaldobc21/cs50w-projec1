e95324211ea66f51fc18fa6631ad065931a44c9e
Ronaldobc21 <76532094+Ronaldobc21@users.noreply.github.com>
2021-09-03 13:49:13 -0600

    Add files via upload

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, session, render_template, request, url_for
+from flask import Flask, session, render_template, request, url_for, redirect, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 
 app = Flask(__name__)
@@ -29,14 +31,59 @@ def index():
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
+
+    session.clear() # Aqui va limpiar la sesion de inicio.
+    if request.method == "POST":
+        if not request.form.get("username"):
+            raise RuntimeError("Must provide username", 403)
+        
+        elif not request.form.get("password"):
+            raise RuntimeError("Must provide password", 403)
+
+        table = db.execute("SELECT * FROM users WHERE ")
+        
     return render_template('login.html')
 
 
 @app.route('/register',methods = ['POST', 'GET'])
 def register():
-    return render_template('register.html')
+
+    session.clear()
+    if request.method == "POST":
+        if not request.form.get("username"):
+            raise RuntimeError("Must provide Username", 400)
+
+        if not request.form.get("password"):
+            raise RuntimeError ("Must provide password", 400)
+
+        if not request.form.get("confirmation"):
+            raise RuntimeError ("Must provide confirmation", 400)
+
+        elif not request.form.get("password") == request.form.get("confirmation"):
+            raise RuntimeError ("password doesn't match", 400)
+
+        if len(db.execute("SELECT * FROM users WHERE username= :username", username=request.form.get("username"))) != 0:
+            raise RuntimeError ("username taken :D", 400)
+
+        password = generate_password_hash(request.form.get("password"))
+
+        newUser = db.execute("INSERT INTO users(username, hash) VALUES(:user, :passw) ",
+                             user=request.form.get("username"), passw=password)
+
+        flash("Registered!")
+
+        return redirect("/")
+    else:
+        return render_template("register.html")
 
 
+@app.route('/Results')
+def Results():
+    
+    return render_template('results.html')
+
+
+    
 
 if __name__ == '__main__': #con este archivo que hace el metodo de ejecucion
     app.run(debug=True)
---
+++ import.py
@@ -0,0 +1,7 @@
+import csv
+
+f = open('books.csv')
+books_csv = csv.reader(open('books.csv'))
+for books in books_csv:
+    csv.execute()
+    
